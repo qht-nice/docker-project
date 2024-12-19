4dd3d60928b3aa3f0ea25de95a78595bfcfc3b8f
qht <qht.tech@gmail.com>
2024-12-19 11:25:49 +0700

    added swarm

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.use(
 app.use(express.json());
 
 app.get("/api/v1", (req, res) => {
-    res.send("<h2>My DevOps project $$$$</h2>");
+    res.send("<h2>My DevOps project</h2>");
     console.log("WASSUP!!")
 });
 
